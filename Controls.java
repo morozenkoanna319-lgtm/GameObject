cf2a021d4e935bab42af89d8929129738071b383
Bebron28 <basurmanovstepan28@gmail.com>
2026-03-28 19:07:07 +0300

    Update Controls.java
    Добавил 90 секунд перезарядки на кнопки SpawnArcher и SpawnTank

--- Controls.java
+++ Controls.java
@@ -6,22 +6,22 @@ public class Controls extends JPanel {
     public Controls(Engine engine) {
         setLayout(new FlowLayout());
 
-        add(new Button("Spawn Base", new GameObject() /* under construction */, () -> {
+        add(new Button("Spawn Base", new GameObject(), () -> {
             System.out.println("spawn Base");
             // under construction
         }));
-        add(new Button("Spawn Archer", new UnitArcher(), () -> { // Добавил
+
+        add(new Button("Spawn Archer", new UnitArcher(), () -> {
             UnitArcher archer = new UnitArcher(1, 50, 800, 50, 500);
             archer.setFraction(1);
             archer.setAttackRange(100);
             archer.setAttackDamage(0);
             engine.spawnObject(archer);
             System.out.println("Archer spawned - will stop and stand");
-        }));
+        }, 90));
 
         add(new Button("Spawn Tank", new UnitDinoRider(), () -> {
             // under construction
-        }));
+        }, 90));
     }
 }
-
